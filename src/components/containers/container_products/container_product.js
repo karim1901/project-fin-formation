b3e9ff977e2d9c4fe3dfc04a3a15e64cf4415f44
karim1901 <karimossama591@gmail.com>
2023-11-17 09:20:05 -1200

    frontend Test

---
+++ src/components/containers/container_products/container_product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Container_product(){
+    return <div className="container_products">
+
+    </div>
+}
+
+
+export default Container_product
---
+++ src/components/containers/container_products/container_update.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Container_update(){
+    return <div className="container_update">
+        
+    </div>
+}
+
+
+export default Container_update
