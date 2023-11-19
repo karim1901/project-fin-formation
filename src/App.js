ea8d66474123bc919cd9866eeff216fd1972a7aa
karim1901 <karimossama591@gmail.com>
2023-11-19 03:07:19 -1200

    create cards ptoducts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
-import React from "react";
-import './App.css' 
-import NavBar from "./components/navBar/navBar";
-import Containers from "./components/containers/container";
+import {Component} from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [],
+    }
+  }
+
+  getData = async () => {
+    const resp = await axios.get("https://jsonplaceholder.typicode.com/photos")
+    this.setState({
+      images: resp.data
+    })
+  }
+
+  ShowComment = async (e)=>{
+    console.log(e.target.id)
+    const req = await axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${e.target.id}`)
+    console.log(req.data)
+
+    const comments =req.data
 
 
+    document.querySelector(e.target.id).innerHtml = comments.
 
-function App() {
-  return (
-    <div className="App">
-      < NavBar/>
-      <Containers/>
+
+    
+  }
+
+  render() {
+    return <div>
+
+
+      <button onClick={this.getData}>Get Data</button>
+      <div className='imgs'>
+      {
+        this.state.images.splice(0,50).map((item) => {
+          console.log(item.id)
+          return <div key={item.id}  id={item.id} onClick={this.ShowComment}>
+            <img id={item.id} src={item.thumbnailUrl} alt={item.title} />
+            <p>{item.title}</p>
+            <div className={item.id}>
+
+            </div>
+
+          </div>
+        })
+      }
+      </div>
+
     </div>
-  );
+  }
 }
 
 export default App;
